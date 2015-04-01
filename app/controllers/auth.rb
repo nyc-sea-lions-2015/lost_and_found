5b754db31975c38f4d03c25b0de6530817675753
Notika314 <notika314@gmail.com>
2015-04-01 10:47:49 -0400

    authentication done

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,9 +1,9 @@
 get "/authentication/login" do
-   erb :"authentication/login"
+ erb :"authentication/login"
 end
 
 get "/authentication/signup" do
-  erb :"authentication/signup"
+ erb :"authentication/signup"
 end
 
 get "/authentication/logout" do
@@ -12,9 +12,21 @@ get "/authentication/logout" do
 end
 
 post "/authentication/signin" do
-
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    [400,"something went wrong"]
+  end
 end
 
 post "/authentication/register_user" do
-
+   if params[:password] == params[:password_confirm]
+  user = User.create(name: params[:name], email: params[:email], password: params[:password])
+  session[:user_id] = user.id
+  redirect "/"
+  else
+    [400,"something went wrong"]
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $(".login").click(function(e) {
+    e.preventDefault();
+    $("#signin").show();
+    $("#signup").hide();
+  });
+
+
+  $(".register").click(function(e) {
+     e.preventDefault();
+    $("#signup").show();
+    $("#signin").hide();
+  });
 });
